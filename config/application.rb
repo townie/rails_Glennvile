e120ed384499762feb2bbb5e08eb3dc9fa17389a
Keith Webber <kwebber@backupify.com>
2014-06-26 23:40:03 -0400

    precompile assets

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Glenville
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
     
-    config.serve_static_assets = true
+    # config.serve_static_assets = true
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
