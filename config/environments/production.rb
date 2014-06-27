c6343abfb2c157ca234f184c9d02f85b7b142384
Keith Webber <kwebber@backupify.com>
2014-06-26 23:57:13 -0400

    config change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,16 +20,16 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
- config.assets.precompile += %w( asset.js asset.css )
+  config.assets.precompile += %w( asset.js asset.css )
 
-config.serve_static_assets = true
-config.assets.compile = true
+  config.serve_static_assets = true
+  config.assets.compile = true
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+ 
 
   # Generate digests for assets URLs.
   config.assets.digest = true
