0f6123f2a87535ae855e89682463fa47d3a47621
Keith Webber <kwebber@backupify.com>
2014-06-27 02:08:46 -0400

    save point

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -12,6 +12,22 @@ $(document).onScroll(function() {
 
 	});
 
+$(window).scroll(function() {
+  
+  // get the variable of how far we've scrolled from the top
+  var offset = $(window).scrollTop();
+	offset     = offset * 0.4;
+
+  $('.cask').css({
+    '-moz-transform': 'rotate(' + offset + 'deg)',
+    '-webkit-transform': 'rotate(' + offset + 'deg)',
+    '-o-transform': 'rotate(' + offset + 'deg)',
+    '-ms-transform': 'rotate(' + offset + 'deg)',
+    'transform': 'rotate(' + offset + 'deg)',
+  });
+  
+});
+
 
 // $('#container').masonry({
 //   itemSelector: '.menu_item',
